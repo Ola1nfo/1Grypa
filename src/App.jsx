0e13faa9b25211d78aafee8188f4e8e054beaeab
Красько Валентина <t-kraskov@mon.gov.ua>
2025-10-29 12:57:09 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NotFound from './pages/NotFound/NotFound';
-import AddContact from './pages/AddContact/AddContact';
-import EditContact from './pages/EditContact/EditContact';
-import ContactList from './pages/ContactList/ContactList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import NotFound from './pages/NotFound/NotFound'
+import AddContact from './pages/AddContact/AddContact'
+import EditContact from './pages/EditContact/EditContact'
+import ContactList from './pages/ContactList/ContactList'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<ContactList />} />
           <Route path="/add" element={<AddContact />} />
--- src/pages/AddContact/AddContact.jsx
+++ src/pages/AddContact/AddContact.jsx
@@ -1,8 +1,39 @@
+import './AddContact.css'
+
+import * as Yup from 'yup'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 export default function AddContact() {
+  const initialValues ={
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    avatar: '',
+    gender: '',
+    status: '',
+    favorite: '',
+  }
+
   return (
     <>
-      
-    </>
+    <div className="container">
+      <div className="modal-content addPage rounded shadow">
+        <div className="modal-header">
+          <h1 className="text-center">Add new contact</h1>
+        </div>
+        <Formik initialValues={initialValues} validationScema={validationScema} onSubmit={ handleSubmit}>
+          {({isSubmitting}) => (
+              <Form>
+                <div className="container">
+                  .
+                </div>
+              </Form>
+             )}
 
+        </Formik>
+      </div>
+    </div>
+
+       </>
   )
 }
